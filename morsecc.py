c143cc334bb0e6820cf03163f515ec730f1ec1e1
Soxfmr <soxfmr@foxmail.com>
2016-04-23 15:15:29 +0800

    argument and help document

--- morsecc.py
+++ morsecc.py
@@ -1,6 +1,10 @@
 # -*- coding:  utf-8 -*-
 import wave
 import sys
+import argparse
+from os import R_OK as MODE_READ
+from os import access
+from os.path import exists
 from core.GlobalConfig import GlobalConfig
 from core.MorseCodeAudio import MorseCodeAudio
 from core.Translator import Translator as MorseCodeTranslator
@@ -62,26 +66,55 @@ def binarize(data, frames, options):
     return data
 
 def main():
+    parser = argparse.ArgumentParser(prog='morsecc',
+        usage='%(prog)s [options] <file>',
+        description='Convert the wave audio file to Morse Code')
+    parser.add_argument('-b', '--base-vision', metavar='<N>', dest='base_vision', type=int, default=0x80,
+        help='The base vision line of the wave audio, for 8-bits rates audio that is 0x80 by default.')
+    parser.add_argument('-m', '--max-division', metavar='<N>', dest='max_division', type=int, default=3,
+        help='The mute segment has no any sounds but with the noise bit closed to the value of \
+        the base vision line, this value determine the max value of the noisze in one bit, default to 3.')
+    parser.add_argument('file')
+
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s v0.2 Beta')
+    args = parser.parse_args()
+
+    # Setup the environment
+    GlobalConfig.BASE_VISION = args.base_vision
+    GlobalConfig.MAX_DIVISION = args.max_division
+
     config = GlobalConfig()
     try:
-        handle = wave.open(sys.argv[1], 'rb')
-        frames = handle.getnframes()
-        data = handle.readframes(frames)
-        # 转换为数值数组
-        data = bytearray(data)
-        # 二值化
-        binary = binarize(data, frames, config)
-        # 测量宽度
-        mcAudio = measure(binary)
-        # 转换为字符
-        translator = translate(mcAudio)
+        if not exists(args.file):
+            raise Exception('File %s not found.' % args.file)
 
-        print '[+] Morse Code: ', translator.morse_code
-        print '[+] Plain Text: ', translator.plain_text
+        if not access(args.file, MODE_READ):
+            raise Exception('Cannot access %s. Permission deined.' % args.file)
+
+        try:
+            handle = wave.open(args.file, 'rb')
+            if handle == None:
+                raise Exception('Invalid format of the wave audio file %s' % args.file)
+
+            frames = handle.getnframes()
+            data = handle.readframes(frames)
+            # 转换为数值数组
+            data = bytearray(data)
+            # 二值化
+            binary = binarize(data, frames, config)
+            # 测量宽度
+            mcAudio = measure(binary)
+            # 转换为字符
+            translator = translate(mcAudio)
+
+            print '[+] Morse Code: ', translator.morse_code
+            print '[+] Plain Text: ', translator.plain_text
+        except Exception as e:
+            print '[-] Error: ', e.message
+        finally:
+            if handle != None: handle.close()
     except Exception as e:
         print '[-] Error: ', e.message
-    finally:
-        if handle != None: handle.close()
 
 if __name__ == '__main__':
     main()
